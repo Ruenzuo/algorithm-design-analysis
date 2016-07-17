ac40582fef08b461e16e775e54bf8c3d0c0ddfcb
Renzo Crisostomo <ruenzuo@gmail.com>
2016-07-17 20:48:01 +0200

    Implement inversion count

--- inversions.rb
+++ inversions.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 
-def merge(first_half, second_half)
+def merge_and_count(first_half, second_half)
   merge = []
+  inversions = 0
   while !first_half.empty? and !second_half.empty?
     if first_half.first <= second_half.first
       merge << first_half.shift
     else
       merge << second_half.shift
+      inversions += first_half.size
     end
   end
   while !first_half.empty?
@@ -15,24 +17,25 @@ def merge(first_half, second_half)
   while !second_half.empty?
     merge << second_half.shift
   end
-  return merge
+  return merge, inversions
 end
 
-def sort(input)
+def sort_and_count(input)
   if input.length <= 1
-    return input
+    return input, 0
   end
   half = input.size / 2
   first_half = input.slice(0, half)
   second_half = input.slice(half, input.length - half)
-  first_half_sorted = sort(first_half)
-  second_half_sorted = sort(second_half)
-  return merge(first_half_sorted, second_half_sorted)
+  first_half_sorted, first_half_count = sort_and_count(first_half)
+  second_half_sorted, second_half_count = sort_and_count(second_half)
+  merge_sorted, merge_count = merge_and_count(first_half_sorted, second_half_sorted)
+  return merge_sorted, first_half_count + second_half_count + merge_count
 end
 
 data_set = File.read('inversions-dataset.txt')
 input = data_set.split("\n").map { |item| item.to_i }
 
-output = sort(input)
+output, count = sort_and_count(input)
 
-puts output
+puts count
